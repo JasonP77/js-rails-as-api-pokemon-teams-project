8ded59cc7d3679f512b8ebe807c38ded1194b650
JasonP77 <llvslr77@gmail.com>
2020-03-18 14:23:15 -0400

    backend

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -21,19 +21,55 @@ function trainerCards(trainer) {
 	let pokemonBtn = document.createElement('button');
 	pokemonBtn.innerText = "Add Pokemon"
 	pokemonBtn.addEventListener('click', addPokemon);
+	// let pokemonList = document.createElement('p');
+	let pokemonContainer = document.createElement('ul')
+	
+		trainer.pokemon.forEach(pokemon => {
+		let pokemonElement = document.createElement('li');
+		pokemonElement.dataset.id = pokemon.id;
+		pokemonElement.innerText = `${pokemon.nickname} (${pokemon.species})`
+		let deleteBtn = document.createElement('button');
+		deleteBtn.addEventListener('click', deleteFn);
+		deleteBtn.innerText = "release"
+		deleteBtn.classList.add('release');
+		
+		pokemonElement.appendChild(deleteBtn);
+		pokemonContainer.appendChild(pokemonElement)
+	});
+
+	// pokemonList.appendChild(pokemonContainer);
 	trainerCard.appendChild(trainerName);
 	trainerCard.appendChild(pokemonBtn);
+	trainerCard.appendChild(pokemonContainer);
 	trainerArea.appendChild(trainerCard);
 	
 }
 
+function deleteFn(event) {
+	fetch(`http://localhost:3000/pokemons/${event.target.parentElement.dataset.id}`, {
+		method: "DELETE"
+	})
+	event.target.parentElement.remove();
+}
+
 function addPokemon(event) {
-	// debugger
-	let pokemonArea = document.createElement('div');
 	fetch("http://localhost:3000/pokemons", {
 		method: 'POST',
 		headers: {"Content-Type": "application/json"},
-		body: JSON.stringify({"trainer_id": `${event.target.parentElement.dataset.id}`})
+		body: JSON.stringify({"trainer_id": `${event.target.parentElement.dataset.id}`}) 
 	})
-	
+	.then(response => response.json())
+	.then(pokemon => {
+		let pokemonArea = event.target.parentElement.querySelector('ul');
+		let pokemonElement = document.createElement('li');
+		pokemonElement.dataset.id = pokemon.id;
+		pokemonElement.innerText = `${pokemon.nickname} (${pokemon.species})`
+		let deleteBtn = document.createElement('button');
+		deleteBtn.addEventListener('click', deleteFn);
+		deleteBtn.innerText = "release"
+		deleteBtn.classList.add('release');
+		
+		pokemonElement.appendChild(deleteBtn);
+		pokemonArea.appendChild(pokemonElement)
+	})
 }
